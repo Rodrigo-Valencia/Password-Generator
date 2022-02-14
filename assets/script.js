a68602a84809a62d5a7466f134a5b2566e1f8bb4
Rodrigo <rodvalenciaa@gmail.com>
2022-02-13 17:15:03 -0700

    character prompt

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,60 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+    var promptAmount = window.prompt('How many characters would you like? Minimum 8, maximum 128.');
+    if (promptAmount === "" || promptAmount === null) {
+        window.alert('You need to provide a valid answer! Please try again.');
+
+        return writePassword();
+    }
+    
+    function specialCharacters(){
+        var promptspecialCharacters = window.prompt('Would you like special characters?');
+        if (promptspecialCharacters === "" || promptspecialCharacters === null) {
+            window.alert('You need to provide a valid answer! Please try again.');
+            return specialCharacters();
+        }
+    }
+    
+    
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+}
+
+var characterAmount = function() {
+    var promptAmount = window.prompt('How many characters would you like?');
+
+    if (promptAmount === "" || promptAmount === null) {
+        window.alert('You need to provide a valid answer! Please try again.');
+        return characterAmount();
+    }
+
+    if (promptAmount === "< min" || promptAmount === "> max") {
+        window.alert('Character amount must be between 8 and 128! Please try again.');
+    }
+
+}
+
+var numberInfo = {
+    min: 8,
+    max: 128
+}
+
