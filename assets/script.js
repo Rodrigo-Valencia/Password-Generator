a3821d0b6d51c5b123db674e956132d9968df24f
Rodrigo <rodvalenciaa@gmail.com>
2022-02-13 19:01:16 -0700

    added array

--- assets/script.js
+++ assets/script.js
@@ -2,61 +2,18 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-    var promptAmount = window.prompt('How many characters would you like? Minimum 8, maximum 128.');
-    if (promptAmount === "" || promptAmount === null) {
-        window.alert('You need to provide a valid answer! Please try again.');
+function writePassword(){
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-        return writePassword();
-    }
-    
-    function specialCharacters(){
-        var promptspecialCharacters = window.prompt('Would you like special characters?');
-        if (promptspecialCharacters === "" || promptspecialCharacters === null) {
-            window.alert('You need to provide a valid answer! Please try again.');
-            return specialCharacters();
-        }
-    }
-    
-    
-
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+    passwordText.value = password
 }
 
-
-
-
+// Array
+var numbers = ['1','2','3','4','5','6','7','8','9','0'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var specialCharacters = ['!' , '"' , '#' , '$' , '%' , '&' , "'" , '(' , ')' , '*' , '+' , ',' , '-' , '.' , '/' , ':' , ';' , '<' , '=' , '>' , '?' , '@' , '[' , ']' , "\ " , '^' , "_" , '`' , '{' , '}' , '|' , '~' ];
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-var randomNumber = function(min, max) {
-    var value = Math.floor(Math.random() * (max - min + 1) + min);
-
-    return value;
-}
-
-var characterAmount = function() {
-    var promptAmount = window.prompt('How many characters would you like?');
-
-    if (promptAmount === "" || promptAmount === null) {
-        window.alert('You need to provide a valid answer! Please try again.');
-        return characterAmount();
-    }
-
-    if (promptAmount === "< min" || promptAmount === "> max") {
-        window.alert('Character amount must be between 8 and 128! Please try again.');
-    }
-
-}
-
-var numberInfo = {
-    min: 8,
-    max: 128
-}
-
